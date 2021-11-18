17c9f58a7627def3cb451b1a7bcc460674fc0f3b
gbubemismith <gsmithwalter@gmail.com>
2021-11-19 00:12:21 +0100

    got tired from work:::can't see clearly

---
+++ example.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gbubemismith/interview-accountapi-/accounts"
+
+func main() {
+	//create new account data
+	accountData := &accounts.AccountData{
+		Data: &accounts.Data{
+			ID: "ad27e265-9605-4b4b-a0e5-3003ea9cc4dd",
+			Type: "accounts",
+			Version: 1,
+
+		},
+	}
+}
